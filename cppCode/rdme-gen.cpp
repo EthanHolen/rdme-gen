08c9bd9f6251063c7532517c36d68614775b8027
Ethan <ethanholen@gmail.com>
2020-08-09 23:53:05 -0600

    determine target path

--- cppCode/rdme-gen.cpp
+++ cppCode/rdme-gen.cpp
@@ -2,6 +2,8 @@
 
 #include <iostream>
 #include <fstream>
+//#include <filesystem>
+#include <unistd.h>
 #include <string.h>
 #include <sys/stat.h>
 
@@ -12,10 +14,14 @@ bool img = false;
 bool fillable = false;
 bool path = false;
 bool help = false;
-string targetPath;
+string target_path;
+string bad_target_path;
+string rdme_gen_folder = string(getenv("HOME")) + "./rdme-gen";
 
 void processArgs(int argc, const char *argv[]);
 bool pathExists(const string &s);
+void determinePaths(string *rdme_path, string *img_path);
+string get_current_dir();
 
 
 
@@ -32,9 +38,9 @@ int main(int argc, const char *argv[])
     // only present help message when -h is passed and dont run the rest of the function
     if (help)
         return 0;
-    if (!pathExists(targetPath) && path)
+    if (target_path == "" && path)
     {
-        printf("The file path %s does not exist", targetPath.c_str());
+        printf("The file path \"%s\" does not exist.\n", bad_target_path.c_str());
         return 0;
     }
 
@@ -42,8 +48,15 @@ int main(int argc, const char *argv[])
 
     // determine paths
 
+    string rdme_path = rdme_gen_folder;
+    string img_path = rdme_gen_folder;
 
 
+    determinePaths(&rdme_path, &img_path);
+    
+
+
+    target_path = get_current_dir();
 
     // copy
 
@@ -52,17 +65,19 @@ int main(int argc, const char *argv[])
 
     // Debugging
     cout
+        << "last argument: " << argv[argc - 1] << endl
         << "Images: " << img << endl
+        << "rdme_gen_folder: " << rdme_gen_folder << endl
+        << "rdme_path: " << rdme_path << endl
+        << "img_path: " << img_path << endl
         << "Path: " << path << endl
-        << "Target Path: " << targetPath << endl
+        << "Target Path: " << target_path << endl
         << "Fillable: " << fillable << endl
         << "Help: " << help << endl;
 
 
     return 1;
 }
-
-
 
 
 void processArgs(int argc, const char *argv[])
@@ -86,8 +101,18 @@ void processArgs(int argc, const char *argv[])
         else if (argument == "-p" || argument == "--path")
         {
             path = true;
-            //TODO: fix this so it dosent segfault and says "you did not provide a path"
-            targetPath = string(argv[i + 1]);
+
+            if (string(argv[argc - 1]) != "-p" && string(argv[argc - 1]) != "--path")
+            {
+                bad_target_path = argv[i + 1];
+
+                if (pathExists(string(argv[i + 1])))
+                    target_path = argv[i + 1];
+            }
+            else
+                target_path = "";
+
+
         }
 
     }
@@ -100,4 +125,30 @@ bool pathExists(const string &s)
 {
     struct stat buffer;
     return (stat(s.c_str(), &buffer) == 0);
+}
+
+
+
+void determinePaths(string *rdme_path, string *img_path)
+{
+
+    if (img)
+    {
+        *rdme_path += "/default-with-images/mainTemplate.md";
+        *img_path += "//default-with-images/rdme-images";
+    }
+    else
+    {
+        /* code */
+    }
+    
+}
+
+
+
+string get_current_dir() {
+   char buff[FILENAME_MAX]; //create string buffer to hold path
+   getcwd( buff, FILENAME_MAX );
+   string current_working_dir(buff);
+   return current_working_dir;
 }
