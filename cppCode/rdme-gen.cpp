440c382117812aab50de117678aaf3f3e093da45
Ethan <ethanholen@gmail.com>
2020-08-13 21:51:33 -0600

    copy not working

--- cppCode/rdme-gen.cpp
+++ cppCode/rdme-gen.cpp
@@ -1,6 +1,7 @@
 // rdme-gen in cpp
 
 #include <iostream>
+#include <copyfile.h>
 #include <fstream>
 #include <unistd.h>
 #include <string.h>
@@ -18,9 +19,11 @@ string bad_target_path;
 string rdme_gen_folder = string(getenv("HOME")) + "./rdme-gen";
 
 void processArgs(int argc, const char *argv[]);
+void help_message(int argc, const char *argv[]);
 bool pathExists(const string &s);
 void determinePaths(string *rdme_path, string *img_path, string *target_path);
 string get_current_dir();
+void copyThis(string from, string to);
 
 int main(int argc, const char *argv[])
 {
@@ -45,17 +48,19 @@ int main(int argc, const char *argv[])
 
     // copy
 
+    copyThis(rdme_path, target_path);
+    // copyfile(rdme_path.c_str(), target_path.c_str(), NULL, COPYFILE_DATA);
+
     // Debugging
     cout
-        << "Images: " << img << endl
-        << "Path: " << path << endl
-        << "Fillable: " << fillable << endl
-        << "Help: " << help << endl
-        << endl
-        << "rdme_gen_folder: " << rdme_gen_folder << endl
-        << "rdme_path: " << rdme_path << endl
-        << "img_path: " << img_path << endl
-        << "Target Path: " << target_path << endl;
+        << "Images: " << img
+        << "\nPath: " << path
+        << "\nFillable: " << fillable
+        << "\nHelp: " << help
+        << "\n\nrdme_gen_folder: " << rdme_gen_folder
+        << "\nrdme_path: " << rdme_path
+        << "\nimg_path: " << img_path
+        << "\nTarget Path: " << target_path << endl;
 
     return 1;
 }
@@ -69,7 +74,7 @@ void processArgs(int argc, const char *argv[])
 
         if (argument == "-h" || argument == "--help")
         {
-            cout << "HELP MESSAGE" << endl;
+            help_message(argc, argv);
             help = true;
         }
         else if (argument == "-img" || argument == "--image")
@@ -114,9 +119,14 @@ void determinePaths(string *rdme_path, string *img_path, string *target_path)
         *rdme_path += "/default/mainTemplate.md";
         *img_path = "";
     }
+
     if (!path)
     {
-        *target_path = get_current_dir();
+        *target_path = get_current_dir() + "/README.md";
+    }
+    else
+    {
+        *target_path += "/README.md";
     }
 }
 
@@ -126,4 +136,41 @@ string get_current_dir()
     getcwd(buff, FILENAME_MAX);
     string current_working_dir(buff);
     return current_working_dir;
+}
+
+void help_message(int argc, const char *argv[])
+{
+    cout
+        << "usage: rdme-gen [-h] [-img] [-f] [-p]\n\n"
+        << "Generate README in current directory.\n\n"
+        << "optional arguments:\n"
+        << "   -h, --help\n"
+        << "   -p , --path           Specify a path for the README other than the current directory\n"
+        << "   -img, --includeImages  Generate the readme with images\n"
+        << "   -f, --fill            Prompt for README info to fill automatically\n";
+    // << "   -fd, --fillDefaults   Prompt for info to set as defaults\n"
+
+    // usage: rdme-gen [-h] [-img] [-f] [-p]
+
+    // Generate README in current directory.
+
+    // optional arguments:
+    //   -h, --help            show this help message and exit
+    //   -p , --path           Specify a path for the README other than the
+    //                         current directory
+
+    //   -img, --includeImages
+    //                         Generate the readme with images
+    //   -f, --fill            Prompt for README info to fill automatically";
+}
+
+void copyThis(string from, string to)
+{
+    ifstream in(from.c_str());
+    ofstream out(to.c_str());
+    string s;
+    while(getline(in, s))
+        out << s << "\n";
+
+    // dst << src.rdbuf();
 }
